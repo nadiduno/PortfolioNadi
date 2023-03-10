88f8aaf24dd7bb0461cadc79ffafb54c8fa0114a
nadiduno <nadiduno@gmail.com>
2023-03-10 15:15:56 -0300

    Update Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,20 +7,14 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img
         className={styles.cover}
-        src="https://github.com/nadiduno/post/blob/main/src/assets/frontendNadiDuno.jpg?raw=true"
-        alt="Foto de Menina no computador"
+        src="https://github.com/nadiduno/PortfolioNadi/blob/main/src/assets/nadiCode.png?raw=true"
+        alt="Foto de Nadi Duno, Nadi é uma mulher, parda de cabelo curto, a foto esta em branco e preto, com um circulo verde neon com codigos de programação"
       />
       <div className={styles.profile}>
-        <Avatar src="https://github.com/nadiduno.png" hasBorder />
+        
         <strong>Nadi Duno</strong>
         <span>DevRel | Web Developer</span>
       </div>
-      <footer>
-        <a href="#">
-          <PencilLine size={20} />
-          Editar seu perfil
-        </a>
-      </footer>
     </aside>
   )
 }
