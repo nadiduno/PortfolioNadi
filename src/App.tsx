9a90da4f4a6114c57301f87f3bc15d684599b1a8
nadiduno <nadiduno@gmail.com>
2023-03-13 12:39:37 -0300

    Add Application

--- src/App.tsx
+++ src/App.tsx
@@ -8,46 +8,46 @@ const appsme: AppMeType[] = [
   {
     id: 1,
     title: 'DevPost',
-    about: 'Uma aplicação para criar postagem e comentarios',
+    about: 'Criar postagem e comentarios',
     urlApp: {
-      photo: 'https://github.com/nadiduno/post/blob/main/.github/ProjectNadiDunoDesenvolvedorFrontEnd.png?raw=true',
+      photo: 'https://github.com/nadiduno/PortfolioNadi/blob/main/.github/App1.png?raw=true',
       deploy: 'https://post-nadiduno.vercel.app/',
       github: 'https://github.com/nadiduno/post',
     },
-    publishedAt: new Date('2023-02-26 13:34:00'),
+    publishedAt: new Date('2023-02-27 13:34:00'),
   },
   {
     id: 2,
     title: 'Gallery Img',
     about: 'Consumindo a API de Unsplash',
     urlApp: {
-      photo: 'https://github.com/nadiduno/post/blob/main/.github/ProjectNadiDunoDesenvolvedorFrontEnd.png?raw=true',
+      photo: 'https://github.com/nadiduno/PortfolioNadi/blob/main/.github/App2.png?raw=true',
       deploy: 'https://gallery-unsplash-nadiduno.vercel.app/',
       github: 'https://github.com/nadiduno/GalleryUnsplash',
     },
-    publishedAt: new Date('2023-02-26 13:34:00'),
+    publishedAt: new Date('2022-12-26 13:34:00'),
   },
   {
     id: 3,
     title: 'E-Commerce',
     about: 'Venda de canecas e camisetas',
     urlApp: {
-      photo: 'https://github.com/nadiduno/post/blob/main/.github/ProjectNadiDunoDesenvolvedorFrontEnd.png?raw=true',
+      photo: 'https://github.com/nadiduno/PortfolioNadi/blob/main/.github/App3.png?raw=true',
       deploy: 'https://ecommercemarmitech.vercel.app/',
       github: 'https://github.com/nadiduno/ecommercemarmitech',
     },
-    publishedAt: new Date('2023-02-26 13:34:00'),
+    publishedAt: new Date('2022-08-31 13:34:00'),
   },
   {
     id: 4,
     title: 'Finance',
     about: 'Registro de finanças pessoal',
     urlApp: {
-      photo: 'https://github.com/nadiduno/post/blob/main/.github/ProjectNadiDunoDesenvolvedorFrontEnd.png?raw=true',
+      photo: 'https://github.com/nadiduno/PortfolioNadi/blob/main/.github/App4.png?raw=true',
       deploy: 'https://financeperson.vercel.app/',
       github: 'https://github.com/nadiduno/financeperson',
     },
-    publishedAt: new Date('2023-02-26 13:34:00'),
+    publishedAt: new Date('2022-08-28 13:34:00'),
   },
 ]
 export function App() {
