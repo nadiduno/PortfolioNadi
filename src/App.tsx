79332d743e1f7e50fccc3561df3db05160e715d2
Nadi Duno <nadiduno@gmail.com>
2023-05-26 16:04:30 -0300

    Update date

--- src/App.tsx
+++ src/App.tsx
@@ -19,28 +19,6 @@ const appsme: AppMeType[] = [
   },
   {
     id: 2,
-    title: 'Migrantes',
-    about: 'Dashboard de Empregabilidade',
-    urlApp: {
-      photo: 'https://github.com/nadiduno/PortfolioNadi/blob/main/.github/App8.png?raw=true',
-      deploy: 'https://app.powerbi.com/groups/8c95b6d3-4a45-44e1-be0b-496158e23210/reports/b4f11f14-1657-46ae-beda-8dc71e6b805e/ReportSection?experience=power-bi',
-      github: 'https://github.com/nadiduno/dataPublic',
-    },
-    publishedAt: new Date('2023-05-1 13:34:00'),
-  },
-  {
-    id: 3,
-    title: 'Vila Móveis',
-    about: 'Dashboard de Vendas',
-    urlApp: {
-      photo: 'https://github.com/nadiduno/PortfolioNadi/blob/main/.github/App7.jpg?raw=true',
-      deploy: 'https://app.powerbi.com/view?r=eyJrIjoiODE4YTEyOTAtZjY1YS00Njc0LWEyM2YtNzBhZTQxZjcxMDNkIiwidCI6IjE0YmU1ZTFkLTM2MGYtNDg0Ni1iNGIwLTJlNTkzNzc1NmQwZCJ9',
-      github: 'https://github.com/nadiduno/dataPublic',
-    },
-    publishedAt: new Date('2023-05-1 13:34:00'),
-  },
-  {
-    id: 4,
     title: 'Atacadao das Bebidas',
     about: 'Dashboard de Vendas',
     urlApp: {
@@ -48,7 +26,29 @@ const appsme: AppMeType[] = [
       deploy: 'https://app.powerbi.com/groups/me/reports/dd7fe305-aacd-40f8-88aa-598068c439c7/ReportSection',
       github: 'https://github.com/nadiduno/dataPublic',
     },
-    publishedAt: new Date('2023-05-1 13:34:00'),
+    publishedAt: new Date('2023-05-16 8:00:00'),
+  },
+  {
+    id: 3,
+    title: 'Migrantes',
+    about: 'Dashboard de Empregabilidade',
+    urlApp: {
+      photo: 'https://github.com/nadiduno/PortfolioNadi/blob/main/.github/App8.png?raw=true',
+      deploy: 'https://app.powerbi.com/groups/8c95b6d3-4a45-44e1-be0b-496158e23210/reports/b4f11f14-1657-46ae-beda-8dc71e6b805e/ReportSection?experience=power-bi',
+      github: 'https://github.com/nadiduno/dataPublic',
+    },
+    publishedAt: new Date('2023-2-14 13:34:00'),
+  },
+  {
+    id: 4,
+    title: 'Vila Móveis',
+    about: 'Dashboard de Vendas',
+    urlApp: {
+      photo: 'https://github.com/nadiduno/PortfolioNadi/blob/main/.github/App7.jpg?raw=true',
+      deploy: 'https://app.powerbi.com/view?r=eyJrIjoiODE4YTEyOTAtZjY1YS00Njc0LWEyM2YtNzBhZTQxZjcxMDNkIiwidCI6IjE0YmU1ZTFkLTM2MGYtNDg0Ni1iNGIwLTJlNTkzNzc1NmQwZCJ9',
+      github: 'https://github.com/nadiduno/dataPublic',
+    },
+    publishedAt: new Date('2022-09-23 13:34:00'),
   },
   {
     id: 5,
