b2066ea55623ac8170011b0412ed7e0b48de446f
Nadi Duno <nadiduno@gmail.com>
2023-07-17 13:09:28 -0300

    Update Link

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -15,44 +15,49 @@ export function Sidebar() {
             src="https://github.com/nadiduno/PortfolioNadi/blob/main/src/assets/nadiCode.png?raw=true"
             alt="Foto de Nadi Duno, Nadi é uma mulher, parda de cabelo curto, a foto esta em branco e preto, com um circulo verde neon com codigos de programação"
           />
-          <div >
-          <a 
-            href='https://www.slideshare.net/nadi192002/practica-1-de-photoshop-12676380' 
-            target="_blank"
-            className={styles.socialnetworks} 
-            rel="noreferrer">
-            Criação de Conteúdo
-          </a>
-          <a 
-            href='https://devrelnadiduno.blogspot.com/' 
-            target="_blank"
-            className={styles.socialnetworks} 
-            rel="noreferrer">
-            Diario de Estudo
-          </a>
-          
-          <a 
-            href='https://www.linkedin.com/in/nadiduno/' 
-            target="_blank" 
-            className={styles.socialnetworks}
-            rel="noreferrer">
-            LinkedIn
-          </a>
-          <a 
-            href='https://instagram.com/nadiduno.dev/' 
-            target="_blank" 
-            className={styles.socialnetworks}
-            rel="noreferrer">
-            Instagram
-          </a>            
-          
-          <a 
-            href='https://www.youtube.com/nadiduno' 
-            target="_blank" 
-            className={styles.socialnetworks}
-            rel="noreferrer">
-            Youtube
-          </a>            
+          <div>
+            <a
+              href="https://www.slideshare.net/nadi192002/practica-1-de-photoshop-12676380"
+              target="_blank"
+              className={styles.socialnetworks}
+              rel="noreferrer"
+            >
+              Criação de Conteúdo
+            </a>
+            <a
+              href="https://devrelnadiduno.blogspot.com/2023/06/fronteditor-para-codar.html"
+              target="_blank"
+              className={styles.socialnetworks}
+              rel="noreferrer"
+            >
+              Diario de Estudo
+            </a>
+
+            <a
+              href="https://www.linkedin.com/pulse/boa-pr%25C3%25A1ctica-ao-declarar-vari%25C3%25A1veis-nadi-duno/?trackingId=%2FlmGTflHTPGjt5KlofYHKQ%3D%3D"
+              target="_blank"
+              className={styles.socialnetworks}
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://instagram.com/nadiduno.dev/"
+              target="_blank"
+              className={styles.socialnetworks}
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+
+            <a
+              href="https://www.youtube.com/watch?v=sG0e7vlFRDE&t=32s"
+              target="_blank"
+              className={styles.socialnetworks}
+              rel="noreferrer"
+            >
+              Youtube
+            </a>
           </div>
         </div>
       </div>
