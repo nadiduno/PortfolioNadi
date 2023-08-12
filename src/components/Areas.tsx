9d049a969c0d198c817e65b0a18e18c81dce0b34
Nadi Duno <nadiduno@gmail.com>
2023-08-11 22:05:34 -0300

    UI

--- src/components/Areas.tsx
+++ src/components/Areas.tsx
@@ -1,30 +1,34 @@
-import { Button, Card, Placeholder } from 'react-bootstrap';
-import { ApplicationMe, AppMeType } from "../components/ApplicationMe"
+import { ApplicationMe, AppMeType } from '../components/ApplicationMe'
 import styles from './Areas.module.css'
+import logomongodb from '../assets/logomongodb.svg'
+import logopython from '../assets/logopython.svg'
+import logostyled from '../assets/logostyledcomponent.svg'
+import logotypescript from '../assets/logotypescript.svg'
+import logoreact from '../assets/logoreact.svg'
 
 const appsme: AppMeType[] = [
   {
     id: 1,
-    title: 'Front-end',
-    about: 'Front-end',
+    title: 'Dados',
+    about: 'Análise e depuração de dados,para extrair insights que podem ajudar na tomada de decisões.',
     urlApp: {
-      photo: 'https://github.com/nadiduno/PortfolioNadi/blob/main/.github/Frontend.png?raw=true',
-      deploy: '/frontend',
-      github: '#',
+      photo: 'https://github.com/nadiduno/PortfolioNadi/blob/main/.github/Dados.png?raw=true',
+      deploy: '/data',
+      github: 'Ver meus trabalhos',
     },
     publishedAt: new Date('2023-08-10 23:00:00'),
   },
   {
     id: 2,
-    title: 'Dados',
-    about: 'Dados',
+    title: 'Front-end',
+    about: 'Paixão por escrever códigos eficientes usáveis seguindo os critérios de acessibilidade.',
     urlApp: {
-      photo: 'https://github.com/nadiduno/PortfolioNadi/blob/main/.github/Dados.png?raw=true',
-      deploy: '/data',
-      github: '#',
+      photo: 'https://github.com/nadiduno/PortfolioNadi/blob/main/.github/Frontend.png?raw=true',
+      deploy: '/frontend',
+      github: 'Ver meus trabalhos',
     },
     publishedAt: new Date('2023-08-10 23:00:00'),
-  },
+  }
 ]
 
 export function Areas() {
@@ -33,22 +37,27 @@ export function Areas() {
       <main className={styles.wrapper}>
         {appsme.map((appme) => {
           return (
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={appme.urlApp.photo} />
-              <Card.Body>
-                <Card.Title>{appme.title}</Card.Title>
-                <Card.Text>
-                  {appme.about}
-                </Card.Text>
+            <div className={styles.boxmain}>
+              <div className={styles.box1}>
+                <img src={appme.urlApp.photo} alt="" />
+              </div>
+              <div className={styles.box2}>
+                <p>{appme.title}</p>
+                <p>{appme.about}</p>
+                <p>Tecnologias</p>
+                <div className={styles.logoteach}>
+                  <img src={logoreact} alt="" />
+                </div>
                 <a
                   href={appme.urlApp.deploy}
                   className={styles.socialnetworks}
                   rel="noreferrer"
                 >
-                  {appme.title}
+                  Ver meus trabalhos
                 </a>
-              </Card.Body>
-            </Card>
+              </div>
+
+            </div>
           )
         })}
       </ main>
@@ -56,3 +65,4 @@ export function Areas() {
 
   )
 }
+
